5e54f37b2152ecdcd53edd6d00e00c97212c4258
cogitoergo9@gmail.com <cogitoergo9@gmail.com@c2d64876-7a3f-b1c4-030c-1359365a9e83>
2012-07-02 15:58:10 +0000

    Requerimiento de Administracion

---
+++ NOTARIA/Admin/Cuentas.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cuentas
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.qtbhNavegador = New Qios.DevSuite.Components.QToolBarHost()
+        Me.qtBarNavegador = New Qios.DevSuite.Components.QToolBar()
+        Me.qtiBack = New Qios.DevSuite.Components.QToolItem(Me.components)
+        Me.qtiFwd = New Qios.DevSuite.Components.QToolItem(Me.components)
+        Me.qtiHome = New Qios.DevSuite.Components.QToolItem(Me.components)
+        CType(Me.qtbhNavegador, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.qtbhNavegador.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'qtbhNavegador
+        '
+        Me.qtbhNavegador.ColorScheme.CurrentTheme = "LunaOlive"
+        Me.qtbhNavegador.ColorScheme.InheritCurrentThemeFromGlobal = False
+        Me.qtbhNavegador.ColorScheme.Scope = Qios.DevSuite.Components.QColorSchemeScope.All
+        Me.qtbhNavegador.Controls.Add(Me.qtBarNavegador)
+        Me.qtbhNavegador.Dock = System.Windows.Forms.DockStyle.Top
+        Me.qtbhNavegador.Location = New System.Drawing.Point(0, 0)
+        Me.qtbhNavegador.Name = "qtbhNavegador"
+        Me.qtbhNavegador.PersistGuid = New System.Guid("9eb74d6e-bd1c-4e03-8bbf-49af58d0eda4")
+        Me.qtbhNavegador.Size = New System.Drawing.Size(755, 31)
+        Me.qtbhNavegador.TabIndex = 0
+        Me.qtbhNavegador.Text = "QToolBarHost1"
+        '
+        'qtBarNavegador
+        '
+        Me.qtBarNavegador.ColorScheme.CurrentTheme = "LunaOlive"
+        Me.qtBarNavegador.ColorScheme.InheritCurrentThemeFromGlobal = False
+        Me.qtBarNavegador.ColorScheme.Scope = Qios.DevSuite.Components.QColorSchemeScope.All
+        Me.qtBarNavegador.Location = New System.Drawing.Point(3, 2)
+        Me.qtBarNavegador.Name = "qtBarNavegador"
+        Me.qtBarNavegador.PersistGuid = New System.Guid("e7328632-e103-4463-9d2f-5c64cb40e4f3")
+        Me.qtBarNavegador.RequestedPosition = 3
+        Me.qtBarNavegador.RowIndex = 0
+        Me.qtBarNavegador.Size = New System.Drawing.Size(94, 27)
+        Me.qtBarNavegador.TabIndex = 0
+        Me.qtBarNavegador.Text = "Barra"
+        Me.qtBarNavegador.ToolBarIndex = 0
+        Me.qtBarNavegador.ToolItems.AddRange(New Qios.DevSuite.Components.QMenuItem() {Me.qtiBack, Me.qtiFwd, Me.qtiHome})
+        '
+        'qtiBack
+        '
+        Me.qtiBack.Title = "Atras"
+        Me.qtiBack.ToolTip = "Atras"
+        '
+        'qtiHome
+        '
+        Me.qtiHome.Title = "Inicio"
+        Me.qtiHome.ToolTip = "Inicio"
+        '
+        'Cuentas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(755, 527)
+        Me.Controls.Add(Me.qtbhNavegador)
+        Me.IsMdiContainer = True
+        Me.Name = "Cuentas"
+        Me.Text = "Cuentas"
+        CType(Me.qtbhNavegador, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.qtbhNavegador.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents qtbhNavegador As Qios.DevSuite.Components.QToolBarHost
+    Friend WithEvents qtBarNavegador As Qios.DevSuite.Components.QToolBar
+    Friend WithEvents qtiBack As Qios.DevSuite.Components.QToolItem
+    Friend WithEvents qtiFwd As Qios.DevSuite.Components.QToolItem
+    Friend WithEvents qtiHome As Qios.DevSuite.Components.QToolItem
+End Class
---
+++ NOTARIA/Admin/PerfilCuenta.vb
@@ -0,0 +1,165 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class PerfilCuenta
+
+     Private Sub PerfilCuenta_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim connection As New MySqlConnection(My.Settings.connectionDB)
+
+        Try
+            Dim da As New MySqlDataAdapter(My.Resources.selectApplication, connection)
+            Dim ds As New DataSet
+
+            connection.Open()
+
+            If da.Fill(ds) Then
+
+                dgvApplication.DataSource = ds.Tables(0)
+
+            End If
+
+            dgvApplication.Columns(1).HeaderText = "Modulo"
+
+            dgvApplication.Columns(2).HeaderText = "Activado"
+
+            dgvApplication.Columns(0).Visible = False
+
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        Finally
+            connection.Close()
+
+        End Try
+
+    End Sub
+
+    Private Sub qcbGuardar_ItemActivated(ByVal sender As System.Object, ByVal e As Qios.DevSuite.Components.QCompositeEventArgs) Handles qcbGuardar.ItemActivated
+        Dim idRol As Integer = 0
+
+        'Insert Rol
+        insertRol(My.Resources.insertRol, qcibPerfil.InputBox.Text)
+
+        For Each row As DataGridViewRow In dgvApplication.Rows
+            'Console.WriteLine(row.Cells(0).FormattedValue & " | " & row.Cells(1).FormattedValue)
+            If Convert.ToBoolean(row.Cells(2).FormattedValue) Then
+                Try
+
+                    'Select rol
+                    idRol = getIdRol(My.Resources.selectRolByTipoRol, qcibPerfil.InputBox.Text)
+
+                    'InsertRolApplication
+                    insertRolApplication(My.Resources.insertAplicationRol, idRol, Val(row.Cells(0).FormattedValue))
+
+
+                Catch ex As Exception
+                    Console.WriteLine(ex.Message)
+                End Try
+
+            End If
+
+        Next row
+
+        NOTARIA.MainNotaria.qclmiRole.Enabled = True
+
+        Me.Close()
+
+
+
+
+    End Sub
+
+
+
+    Function insertRol(ByVal query As String, ByVal param As String) As Integer
+        Dim connectDB As New MySqlConnection(My.Settings.connectionDB)
+        Dim cmd As MySqlCommand = New MySqlCommand(My.Resources.insertRol, connectDB)
+        Dim rowEffect As Integer = 0
+
+        Try
+            connectDB.Open()
+
+            cmd.Parameters.AddWithValue("@tipoRol", qcibPerfil.InputBox.Text)
+
+            rowEffect = cmd.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        Finally
+
+            connectDB.Close()
+
+        End Try
+
+        Return rowEffect
+
+    End Function
+
+
+    Function getIdRol(ByVal query As String, ByVal rol As String) As Integer
+        Dim connectDB As New MySqlConnection(My.Settings.connectionDB)
+        Dim cmd As MySqlCommand = New MySqlCommand(My.Resources.selectRolByTipoRol, connectDB)
+        Dim reader As MySqlDataReader
+        Dim idRol As Integer = 0
+
+        Try
+            connectDB.Open()
+
+            cmd.Parameters.AddWithValue("@tipoRol", rol)
+
+            reader = cmd.ExecuteReader
+
+            While reader.Read
+
+                idRol = Val(reader.GetString(0))
+
+            End While
+
+            reader.Close()
+
+        Catch ex As Exception
+
+            Console.WriteLine(ex.Message)
+
+        Finally
+
+            connectDB.Close()
+
+        End Try
+
+        Return idRol
+
+    End Function
+
+
+    Function insertRolApplication(ByVal query As String, ByVal idRol As Integer, ByVal idApp As Integer)
+        Dim connectDB As New MySqlConnection(My.Settings.connectionDB)
+        Dim cmd As MySqlCommand = New MySqlCommand(My.Resources.insertAplicationRol, connectDB)
+        Dim rowEffect As Integer = 0
+
+        Try
+            connectDB.Open()
+
+            cmd.Parameters.AddWithValue("@idRol", idRol)
+
+            cmd.Parameters.AddWithValue("@idAplication", idApp)
+
+            rowEffect = cmd.ExecuteNonQuery()
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        Return rowEffect
+
+    End Function
+
+    Private Sub qcbCancelar_ItemActivated(ByVal sender As System.Object, ByVal e As Qios.DevSuite.Components.QCompositeEventArgs) Handles qcbCancelar.ItemActivated
+
+        NOTARIA.MainNotaria.qclmiRole.Enabled = True
+
+        Me.Close()
+
+    End Sub
+End Class
+

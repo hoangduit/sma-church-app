d7b6d2927b12500472e69e1442e84ab885872aef
cogitoergo9@gmail.com <cogitoergo9@gmail.com@c2d64876-7a3f-b1c4-030c-1359365a9e83>
2013-04-19 21:06:37 +0000

    Modulo LOgin y Admin

--- trunk/NOTARIA/MainNotaria.vb
+++ trunk/NOTARIA/MainNotaria.vb
@@ -1,7 +1,72 @@
-﻿Public Class MainNotaria
+﻿Imports MySql.Data.MySqlClient
+Public Class MainNotaria
+    Public currentPerfil As Integer
+    Private Sub AccessControl()
+        Dim listModule As New List(Of String)
+        Dim connectionMysql As String = My.Settings.connectionDB
+        Dim connect As MySqlConnection = New MySqlConnection(connectionMysql)
+        Dim command As MySqlCommand = New MySqlCommand(My.Resources.selectModuleByIdRol, connect)
+        Dim reader As MySqlDataReader
+        Dim modulo As String = ""
+
+        Try
+            connect.Open()
+
+            command.Parameters.AddWithValue("@tipoRol", currentPerfil)
+
+            reader = command.ExecuteReader
+
+            While reader.Read
+                modulo = reader.GetString(0)
+                listModule.Add(modulo.Trim)
+                'MessageBox.Show("Modulo " + modulo)
+
+            End While
 
 
-   
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        Finally
+            connect.Close()
+
+        End Try
+
+        listModule.Sort()
+
+        If listModule.Count > 0 Then
+
+            For Each page As Qios.DevSuite.Components.Ribbon.QRibbonPage In QRibbon1.Controls
+
+                If listModule.IndexOf(page.Text.Trim) >= 0 Then
+                    page.Enabled = True
+
+
+                Else
+                    page.Enabled = False
+
+                End If
+            Next
+
+        End If
+
+    End Sub
+    'Oculta los modulos
+    Private Sub desactiveAllPage()
+
+        For Each page As Qios.DevSuite.Components.Ribbon.QRibbonPage In QRibbon1.Controls
+
+            page.Ribbon.Visible = False
+
+        Next
+
+    End Sub
+
+    Private Function hasObject(ByVal modulo As String, ByVal name As String) As Boolean
+        Return (modulo.Equals(name))
+    End Function
+
     Private Sub QCompositeButton1_ItemActivated(ByVal sender As System.Object, ByVal e As Qios.DevSuite.Components.QCompositeEventArgs) Handles qcbNuevoPre.ItemActivated
         Dim MDIFormListPreRegistro As New LstPreRegistros
         MDIFormListPreRegistro.MdiParent = Me
@@ -47,6 +112,11 @@
         MDIFormListaPreRegistro.MdiParent = Me
         MDIFormListaPreRegistro.WindowState = 2
         MDIFormListaPreRegistro.Show()
+        'desactiveAllPage()
+        AccessControl()
+
+
+
     End Sub
 
     'Con este metodo se cierran todos los forms hijos en la aplicacion
@@ -59,7 +129,7 @@
         End If
 
     End Sub
- 
+
     Private Sub qrPagePreRegistro_Activated(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles qrPagePreRegistro.Activated
         CloseAllMDIChild()
         'Pendiente encontrar como cargar la lista de preregistros
@@ -81,5 +151,19 @@
         MDIFormEditUser.WindowState = 2
         MDIFormEditUser.Show()
 
+    End Sub
+
+    Private Sub qcbEditRole_ItemActivated(ByVal sender As System.Object, ByVal e As Qios.DevSuite.Components.QCompositeEventArgs) Handles qcbEditRole.ItemActivated
+        Dim MDIFormEditRole As New EditRole
+        CloseAllMDIChild()
+        MDIFormEditRole.MdiParent = Me
+        MDIFormEditRole.WindowState = 2
+        MDIFormEditRole.Show()
+    End Sub
+
+
+
+    Private Sub MainNotaria_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        Application.Exit()
     End Sub
 End Class
